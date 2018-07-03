8dbfaf43ff9cc6262c034b6db2cbf653731e93fc
medi <pprathamesh1594@gmail.com>
2018-07-03 23:43:19 +0530

    initiate the http project

---
+++ 6_dad_jokes/dad_joke.py
@@ -0,0 +1,10 @@
+# This project helps us understand about `requests` modules
+#  in Python
+
+import requests
+import pyfiglet
+import termcolor
+from random import choice
+
+header = pyfiglet.figlet_format("DAD JOKES")
+header =  termcolor.colored(header, color="magnet")
