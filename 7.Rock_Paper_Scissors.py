294b3b47ebdb79f8e3f218ce2a6ea754ab30576f
Pratik Pithore <33758443+Pratik2799@users.noreply.github.com>
2018-10-31 15:16:56 +0530

    add Rock_Paper_Scissors game

---
+++ 7.Rock_Paper_Scissors.py
@@ -0,0 +1,29 @@
+from random import randint
+
+user = input("rock,paper,scissors ?")
+pick = randint(1,3)
+if (pick == 1):
+    computer = "rock"
+    print("0")
+elif (pick == 2):
+    computer = "paper"
+    print("-_-")
+else:
+    computer = "scissors"
+    print("god damn not again -_-")
+if (user == computer):
+    print("DRAW")
+elif(user == "rock" and computer == "paper"):
+    print("player winns!")
+elif(user == "paper" and computer == "rock" ):
+    print("computer wins !")
+elif(user == "scissors" and computer == "paper"):
+    print("player wins !")
+elif(user == "paper" and computer =="scissors"):
+    print("computer wins !")
+elif(user == "scissors" and computer == "rock"):
+    print("computer wins !")
+elif(user == "rock" and computer == "scissors"):
+    print("player wins !")
+else:
+    print("what? :(")
